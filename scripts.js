ec96fca966a2d1692264151720bb93a624df3f68
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2025-03-20 15:04:32 +0500

    init commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelector('.tile-download').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'files/cv.pdf';
+    link.download = 'cv.pdf';
+    link.click();
+});
